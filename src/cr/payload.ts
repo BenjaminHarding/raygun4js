a8ccf560d6e54667e319822647fedd5d93a237cd
BenjaminHarding <benjamin.p.harding@gmail.com>
2018-12-11 10:27:00 +1300

    Create CR payload types

---
+++ src/cr/payload.ts
@@ -0,0 +1,83 @@
+type StackTrace = Partial<{
+    LineNumber: number;
+    ColumnNumber: number;
+    ClassName: string;
+    FileName: string;
+    MethodName: string;
+}>;
+
+type Error = {
+    ClassName: string;
+    Message: string;
+    StackTrace: StackTrace[];
+    StackString: string;
+};
+
+type Environment = {
+    UtcOffset: number;
+    'User-Language': string;
+    'Document-Mode': string;
+    'Browser-Width': number;
+    'Browser-Height': number;
+    'Screen-Width': number;
+    'Screen-Height': number;
+    'Color-Depth': number;
+    Browser: string;
+    'Browser-Name': string;
+    'Browser-Version': string;
+    Platform: string;
+};
+
+type CustomData = {
+    [key: string]: any;
+};
+
+type QueryString = {
+    [key: string]: string;
+};
+
+type Headers = {
+    'User-Agent': string;
+    Referer: string;
+    Host: string;
+};
+
+type Request = {
+    Url: string;
+    QueryString: QueryString;
+    Headers: Headers;
+};
+
+type User = {
+    Identifier: string;
+    IsAnonymous: boolean;
+    Email: string;
+    FullName: string;
+    FirstName: string;
+};
+
+type Breadcrumb = {
+    level: string;
+    timestamp: number;
+    type: string;
+    message: string;
+};
+
+export type Payload = {
+    OccurredOn: Date;
+    Details: {
+        Error: Error;
+        Environment: Environment;
+        User: User;
+        Breadcrumbs?: Breadcrumb[];
+        GroupingKey?: string;
+        Client: {
+            Name: string;
+            Version: string;
+        },
+        UserCustomData: CustomData;
+        Tags: string[];
+        Request: Request;
+        Version: string;
+    };
+};
