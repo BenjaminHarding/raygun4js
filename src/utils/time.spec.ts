9d7155b92a001fb966c05f87eefcc187e076087a
BenjaminHarding <benjamin.p.harding@gmail.com>
2018-12-11 16:34:27 +1300

    Add tests for timestamp method

--- src/utils/time.spec.ts
+++ src/utils/time.spec.ts
@@ -1,3 +1,24 @@
-export function timestamp(hours: number): number {
-    return Date.now() + hours * 60 * 1000;
-}
+import { timestamp } from './time';
+
+describe('Time', () => {
+    describe('timestamp', () => {
+        const dateNowBefore = Date.now;
+        const TIME_NOW = 10;
+        
+        beforeAll(() => {
+            Date.now = jest.fn(() => TIME_NOW);
+        });
+
+        afterAll(() => {
+            Date.now = dateNowBefore;
+        });
+
+        it("returns current timestamp when hours passed are 0", () => {
+            expect(timestamp(0)).toBe(TIME_NOW);
+        });
+
+        it("returns the 4 hours in the future", () => {
+            expect(timestamp(4)).toBe(TIME_NOW + 4 * 60 * 1000);
+        });
+    });
+});
